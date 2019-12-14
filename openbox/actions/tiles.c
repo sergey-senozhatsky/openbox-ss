90e2b30857d2620d2cb35b58291a68309b72eede
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2019-12-14 13:43:48 +0900

    tiles: add SplitTilesRows
    Have user-requested number of tile rows.
    
        <keybind key="W-0x2b">
          <action name="SplitTilesRows">
            <rows>2</rows>
          </action>
        </keybind>
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- openbox/actions/tiles.c
+++ openbox/actions/tiles.c
@@ -6,23 +6,8 @@
 #include "openbox/debug.h"
 #include "openbox/openbox.h"
 
-enum {
-    CURRENT_MONITOR = -1,
-    ALL_MONITORS = -2,
-    NEXT_MONITOR = -3,
-    PREV_MONITOR = -4
-};
-
 typedef struct {
-    GravityCoord x;
-    GravityCoord y;
-    gint w;
-    gint w_denom;
-    gint h;
-    gint h_denom;
-    gint monitor;
-    gboolean w_sets_client_size;
-    gboolean h_sets_client_size;
+	guint num_rows;
 } Options;
 
 static void free_func(gpointer o);
@@ -31,21 +16,30 @@ static gboolean run_split_tiles_vert_func(ObActionsData *data,
 					  gpointer options);
 static gboolean run_focus_tile_vert_func(ObActionsData *data,
 					  gpointer options);
+static gboolean run_split_tiles_rows_func(ObActionsData *data,
+					  gpointer options);
 
 void action_tiles_startup(void)
 {
-    actions_register("SplitTilesVertically", setup_tiles_func,
-		    free_func, run_split_tiles_vert_func);
-    actions_register("FocusTileVerrtically", setup_tiles_func,
-		    free_func, run_focus_tile_vert_func);
+	actions_register("SplitTilesVertically", setup_tiles_func,
+			free_func, run_split_tiles_vert_func);
+	actions_register("FocusTileVerrtically", setup_tiles_func,
+			free_func, run_focus_tile_vert_func);
+	actions_register("SplitTilesRows", setup_tiles_func,
+			free_func, run_split_tiles_rows_func);
 }
 
 static gpointer setup_tiles_func(xmlNodePtr node)
 {
+	xmlNodePtr n;
 	Options *o;
 
 	o = g_slice_new0(Options);
-	o->monitor = CURRENT_MONITOR;
+	if ((n = obt_xml_find_node(node, "rows"))) {
+		gchar *s = obt_xml_node_string(n);
+
+		o->num_rows = strtol(s, &s, 10);
+	}
 	return o;
 }
 
@@ -94,34 +88,97 @@ static gboolean resize_tile(ObClient *client, guint x, guint y,
 		return 0;
 
 	h = adjusted_height(client, h);
-	client_maximize(client, FALSE, 1);
+	client_maximize(client, FALSE, 0);
+	client_maximize(client, FALSE, 2);
 	client_move_resize(client, x, y, w, h);
+
 	return 1;
+}
+
+static gboolean run_split_tiles_rows_func(ObActionsData *data, gpointer options)
+{
+	Options *o = options;
+	GList *it;
+	int num_client = 0;
+	int new_width = 0;
+	int new_height = 0;
+	Size screen_sz = get_screen_physical_size();
+	int new_y = INT_MAX;
+	ObClient *focused = NULL;
+	guint clients_per_row;
+	guint clients_last_row;
+	guint current_row = 0;
+
+	if (!o->num_rows)
+		return 0;
+
+	num_client = enum_clients(&focused, &new_y);
+	if (num_client < 2) {
+		client_maximize(focused, TRUE, 0);
+		return 0;
+	}
+
+	screen_sz.height -= new_y;
+	new_height = screen_sz.height / o->num_rows;
+	clients_per_row = num_client / o->num_rows;
+	if (!clients_per_row)
+		clients_per_row = 1;
+	clients_last_row = clients_per_row;
+	if (num_client % o->num_rows)
+		clients_last_row += 1;
+
+	num_client = 0;
+	for (it = client_list; it; it = g_list_next(it)) {
+		ObClient *client;
+
+		if (current_row == o->num_rows - 1) {
+			new_width = screen_sz.width / clients_last_row;
+			clients_per_row = clients_last_row;
+		} else {
+			new_width = screen_sz.width / clients_per_row;
+		}
+
+		client = it->data;
+		if (!resize_tile(client,
+				num_client * new_width,
+				new_y + current_row * new_height,
+				new_width,
+				new_height))
+			continue;
+
+		num_client++;
+		if (num_client >= clients_per_row) {
+			num_client = 0;
+			current_row++;
+		}
+	}
+	return 0;
 }
 
 static gboolean run_split_tiles_vert_func(ObActionsData *data, gpointer options)
 {
 	GList *it;
-	int cnum = 0;
+	int num_client = 0;
 	int new_width = 0;
 	Size screen_sz = get_screen_physical_size();
 	int new_y = INT_MAX;
 	ObClient *focused = NULL;
 
-	cnum = enum_clients(&focused, &new_y);
-	if (cnum < 2) {
+	num_client = enum_clients(&focused, &new_y);
+	if (num_client < 2) {
 		client_maximize(focused, TRUE, 0);
 		return 0;
 	}
 
-	new_width = screen_sz.width / cnum;
-	cnum = 0;
+	screen_sz.height -= new_y;
+	new_width = screen_sz.width / num_client;
+	num_client = 0;
 	if (focused) {
 		resize_tile(focused,
 			    0, new_y,
 			    new_width,
 			    screen_sz.height - new_y);
-		cnum = 1;
+		num_client = 1;
 	}
 
 	for (it = client_list; it; it = g_list_next(it)) {
@@ -132,10 +189,11 @@ static gboolean run_split_tiles_vert_func(ObActionsData *data, gpointer options)
 			continue;
 
 		if (resize_tile(client,
-				cnum * new_width, new_y,
+				num_client * new_width,
+				new_y,
 				new_width,
-				screen_sz.height - new_y)) {
-			cnum++;
+				screen_sz.height)) {
+			num_client++;
 		}
 	}
 
@@ -145,25 +203,26 @@ static gboolean run_split_tiles_vert_func(ObActionsData *data, gpointer options)
 static gboolean run_focus_tile_vert_func(ObActionsData *data, gpointer options)
 {
 	GList *it;
-	int cnum = 0;
+	int num_client = 0;
 	int new_width = 0;
 	Size screen_sz = get_screen_physical_size();
 	int new_y = INT_MAX;
 	ObClient *focused = NULL;
 
-	cnum = enum_clients(&focused, &new_y);
+	num_client = enum_clients(&focused, &new_y);
 	if (!focused)
 		return 0;
 
-	if (cnum < 2) {
+	if (num_client < 2) {
 		client_maximize(focused, TRUE, 0);
 		return 0;
 	}
 
+	screen_sz.height -= new_y;
 	new_width = screen_sz.width / 2;
-	cnum--;
-	new_width /= cnum;
-	cnum = 0;
+	num_client--;
+	new_width /= num_client;
+	num_client = 0;
 
 	resize_tile(focused, 0, new_y,
 		    screen_sz.width / 2,
@@ -177,11 +236,11 @@ static gboolean run_focus_tile_vert_func(ObActionsData *data, gpointer options)
 			continue;
 
 		if (resize_tile(client,
-				screen_sz.width / 2 + cnum * new_width,
+				screen_sz.width / 2 + num_client * new_width,
 				new_y,
 				new_width,
-				screen_sz.height - new_y)) {
-			cnum++;
+				screen_sz.height)) {
+			num_client++;
 		}
 	}
 
